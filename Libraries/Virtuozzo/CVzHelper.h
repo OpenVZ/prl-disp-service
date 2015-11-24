9112c718eae2e1930a7c1db943edb4006919cc33
Nikita Spiridonov <nspiridonov@odin.com>
2015-11-24 15:33:01 +0400

    Return back get_env_status_by_ctid function
    Return back previously removed get_env_status function (commit
    53464122) which can determine containers status by veid and adapt
    it to veid->ctid changes.
    
    There were two get_env_status functions before veid->ctid changes
    which accept veid (ctid now) and uuid respectively. Lets just return
    back second get_env_status function which accept veid (ctid now) to
    preserve PCS6 behaviour.
    
    Signed-off-by: Nikita Spiridonov <nspiridonov@odin.com>

--- Libraries/Virtuozzo/CVzHelper.h
+++ Libraries/Virtuozzo/CVzHelper.h
@@ -271,6 +271,7 @@ public:
 	 */
 	static int get_vz_config_param(const char *param, QString &out);
 	int get_envid_list(QStringList &lst);
+	static int get_env_status_by_ctid(const QString &ctid, VIRTUAL_MACHINE_STATE &nState);
 	static int get_env_status(const QString &uuid, VIRTUAL_MACHINE_STATE &nState);
 	static Ct::Statistics::Aggregate* get_env_stat(const QString& uuid_);
 	static int get_env_fstat(const QString &uuid, QList<Ct::Statistics::Filesystem>& fs);
