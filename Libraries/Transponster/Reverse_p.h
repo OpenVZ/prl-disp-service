50a06e2f9bde4d1de2256f1c0a63045e74f45931
Dmitry Andreev <dandreev@virtuozzo.com>
2016-07-13 13:07:37 +0300

    Revert "Do not set device address for disks in Transponster #PSBM-49403"
    This reverts commit 8da10f8f46f39e47dddf12c231ba8d8832898d38.

--- Libraries/Transponster/Reverse_p.h
+++ Libraries/Transponster/Reverse_p.h
@@ -634,6 +634,40 @@ private:
 };
 
 ///////////////////////////////////////////////////////////////////////////////
+// struct Attachment
+
+struct Attachment
+{
+	Attachment(): m_ide(0), m_sata(0)
+	{
+	}
+
+	Libvirt::Domain::Xml::VAddress craftIde();
+	Libvirt::Domain::Xml::VAddress craftSata();
+	Libvirt::Domain::Xml::VAddress craftScsi(const boost::optional<Libvirt::Domain::Xml::EModel>& model_);
+	deviceList_type getControllers() const
+	{
+		return m_controllerList;
+	}
+
+private:
+	enum
+	{
+		IDE_UNITS = 2,
+		IDE_BUSES = 2,
+		SATA_UNITS = 6,
+		SCSI_TARGETS = 256
+	};
+
+	void craftController(const Libvirt::Domain::Xml::VChoice589& bus_, quint16 index_);
+
+	quint16 m_ide;
+	quint16 m_sata;
+	QMap<Libvirt::Domain::Xml::EModel, quint16> m_scsi;
+	deviceList_type m_controllerList;
+};
+
+///////////////////////////////////////////////////////////////////////////////
 // struct List
 
 struct List
@@ -674,6 +708,8 @@ struct List
 {
 	List(const CVmSettings& settings_, Device::List& list_);
 
+	const Attachment& getAttachment() const;
+
 	void add(const CVmHardDisk* hdd_, const CVmRunTimeOptions* runtime_);
 	void add(const CVmOpticalDisk* cdrom_);
 	void add(const CVmFloppyDisk* floppy_);
@@ -686,6 +722,7 @@ private:
 private:
 	Reverse m_boot;
 	Device::List& m_deviceList;
+	Attachment m_attachment;
 };
 
 template<class T>
@@ -698,7 +735,22 @@ void List::build(T builder_)
 	builder_.setTarget();
 	builder_.setBackingChain();
 
-	m_deviceList.add(static_cast<const T&>(builder_).getResult());
+	Libvirt::Domain::Xml::Disk d = static_cast<const T&>(builder_).getResult();
+	switch (d.getTarget().getBus().get())
+	{
+	case Libvirt::Domain::Xml::EBusIde:
+		d.setAddress(m_attachment.craftIde());
+		break;
+	case Libvirt::Domain::Xml::EBusSata:
+		d.setAddress(m_attachment.craftSata());
+		break;
+	case Libvirt::Domain::Xml::EBusScsi:
+		d.setAddress(m_attachment.craftScsi(builder_.getModel().getScsiModel()));
+		break;
+	default:
+		break;
+	}
+	m_deviceList.add(d);
 }
 
 } // namespace Boot
