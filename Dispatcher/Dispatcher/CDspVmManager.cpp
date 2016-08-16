11c558e4c929025652372cb459163a5314cd8f70
Dmitry Andreev <dandreev@virtuozzo.com>
2016-08-16 18:05:37 +0300

    Status waiting on VM pause #PSBM-51013
    VM pause request returns before VM pause state is reached. Any immediate
    resume command will fail. With this patch pause request will be waiting for
    the appropriate VM state.

--- Dispatcher/Dispatcher/CDspVmManager.cpp
+++ Dispatcher/Dispatcher/CDspVmManager.cpp
@@ -1711,7 +1711,8 @@ Dispatcher::Dispatcher()
 	m_map[PVE::DspCmdVmStartVNCServer] = map(Tag::Simple<PVE::DspCmdVmStartVNCServer>());
 	m_map[PVE::DspCmdVmStopVNCServer] = map(Tag::Simple<PVE::DspCmdVmStopVNCServer>());
 	m_map[PVE::DspCmdVmReset] = map(Tag::Fork<Tag::Reply<Essence<PVE::DspCmdVmReset> > >());
-	m_map[PVE::DspCmdVmPause] = map(Tag::Fork<Tag::Reply<Essence<PVE::DspCmdVmPause> > >());
+	m_map[PVE::DspCmdVmPause] = map(Tag::Fork<Tag::State<Essence<PVE::DspCmdVmPause>,
+		Vm::Fork::State::Strict<VMS_PAUSED> > >());
 	m_map[PVE::DspCmdVmSuspend] = map(Tag::Fork<Tag::State<Essence<PVE::DspCmdVmSuspend>,
 		Vm::Fork::State::Strict<VMS_SUSPENDED> > >());
 	m_map[PVE::DspCmdVmDropSuspendedState] = map(Tag::Fork<Tag::Reply<Essence<PVE::DspCmdVmDropSuspendedState> > >());
