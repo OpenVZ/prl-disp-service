cfb8865dcaf452262ee110b5e251119178f06dc9
Aleksandr Alekseev <alexander.alekseev@virtuozzo.com>
2020-11-10 16:26:14 +0300

    Add no-ipv6-filters for VMs with fw=on

--- Libraries/Transponster/Reverse.cpp
+++ Libraries/Transponster/Reverse.cpp
@@ -3385,41 +3385,119 @@ QList<Libvirt::Filter::Xml::VChoice5120> Reverse::prepareDefaultDeny(Libvirt::Fi
 	return result;
 }
 
+boost::optional<Libvirt::Filter::Xml::VChoice5120>
+Reverse::prepareIpSpoofing(const CVmGenericNetworkAdapter &adapter)
+{
+	static QString S_NO_IP_SPOOFING = "no-ip-spoofing";
+	static QString S_IPV4_PARAMETER_NAME = "IP";
+	Transponster::Device::Network::View view(adapter);
+	if (!adapter.getPktFilter()->isPreventIpSpoof() ||
+				adapter.isConfigureWithDhcp() || view.getIpv4().empty())
+		return boost::none;
+	
+	Libvirt::Filter::Xml::FilterrefNodeAttributes filterref;
+	filterref.setFilter(S_NO_IP_SPOOFING);
+	QList<Libvirt::Filter::Xml::Parameter> plist;
+	Libvirt::Filter::Xml::Parameter p;
+	foreach (const QString& ip, view.getIpv4()) {
+		p.setName(S_IPV4_PARAMETER_NAME);
+		p.setValue(ip.split('/').first());
+		plist.append(p);
+	}
+	filterref.setParameterList(plist);
+	mpl::at_c<Libvirt::Filter::Xml::VChoice5120::types, 0>::type filterref_holder;
+	filterref_holder.setValue(filterref);
+	return Libvirt::Filter::Xml::VChoice5120(filterref_holder);
+}
+
+boost::optional<Libvirt::Filter::Xml::VChoice5120>
+Reverse::prepareIpv6Spoofing(const CVmGenericNetworkAdapter &adapter)
+{
+	static QString S_NO_IPV6_SPOOFING = "no-ipv6-spoofing";
+	static QString S_IPV6_PARAMETER_NAME = "IPV6";
+	Transponster::Device::Network::View view(adapter);
+	if (!adapter.getPktFilter()->isPreventIpSpoof() ||
+			adapter.isConfigureWithDhcpIPv6() || view.getIpv6().empty())
+		return boost::none;
+
+	Libvirt::Filter::Xml::FilterrefNodeAttributes filterref;
+	filterref.setFilter(S_NO_IPV6_SPOOFING);
+	QList<Libvirt::Filter::Xml::Parameter> plist;
+	Libvirt::Filter::Xml::Parameter p;
+	foreach (const QString& ip, view.getIpv6()) {
+		p.setName(S_IPV6_PARAMETER_NAME);
+		p.setValue(ip.split('/').first());
+		plist.append(p);
+	}
+	filterref.setParameterList(plist);
+	mpl::at_c<Libvirt::Filter::Xml::VChoice5120::types, 0>::type filterref_holder;
+	filterref_holder.setValue(filterref);
+	return Libvirt::Filter::Xml::VChoice5120(filterref_holder);		
+}
+
+boost::optional<Libvirt::Filter::Xml::VChoice5120>
+Reverse::prepareMacSpoofing(const CVmGenericNetworkAdapter &adapter)
+{
+	static QString S_NO_MAC_SPOOFING = "no-mac-spoofing";
+	static QString S_MAC_PARAMETER_NAME = "MAC";
+
+	if (!adapter.getPktFilter()->isPreventMacSpoof())
+		return boost::none;
+
+	Transponster::Device::Network::View view(adapter);
+	Libvirt::Filter::Xml::FilterrefNodeAttributes filterref;
+	filterref.setFilter(S_NO_MAC_SPOOFING);
+	QList<Libvirt::Filter::Xml::Parameter> plist;
+	Libvirt::Filter::Xml::Parameter p;
+	p.setName(S_MAC_PARAMETER_NAME);
+	p.setValue(view.getMac());
+	plist.append(p);
+	filterref.setParameterList(plist);
+	mpl::at_c<Libvirt::Filter::Xml::VChoice5120::types, 0>::type filterref_holder;
+	filterref_holder.setValue(filterref);
+	return Libvirt::Filter::Xml::VChoice5120(filterref_holder);
+}
+
+boost::optional<Libvirt::Filter::Xml::VChoice5120>
+Reverse::preparePromisc(const CVmGenericNetworkAdapter &adapter)
+{
+	static QString S_NO_PROMISC = "no-promisc";
+	static QString S_MAC_PARAMETER_NAME = "MAC";
+
+	if (!adapter.getPktFilter()->isPreventPromisc())
+		return boost::none;
+
+	Transponster::Device::Network::View view(adapter);
+	Libvirt::Filter::Xml::FilterrefNodeAttributes filterref;
+	filterref.setFilter(S_NO_PROMISC);
+	QList<Libvirt::Filter::Xml::Parameter> plist;
+	Libvirt::Filter::Xml::Parameter p;
+	p.setName(S_MAC_PARAMETER_NAME);
+	p.setValue(view.getMac());
+	plist.append(p);
+	filterref.setParameterList(plist);
+	mpl::at_c<Libvirt::Filter::Xml::VChoice5120::types, 0>::type filterref_holder;
+	filterref_holder.setValue(filterref);
+	return Libvirt::Filter::Xml::VChoice5120(filterref_holder);
+}
+
+
 QList <Libvirt::Filter::Xml::VChoice5120>
 Reverse::prepareNetFilters(const CVmGenericNetworkAdapter &adapter)
 {
 	QList <Libvirt::Filter::Xml::VChoice5120> result;
+	
+	boost::optional<Libvirt::Filter::Xml::VChoice5120> c;
+	if (c = prepareIpSpoofing(adapter))
+		result.append(c.get());
+	if (c = prepareIpv6Spoofing(adapter)) 
+		result.append(c.get());
+	if (c = prepareMacSpoofing(adapter))
+		result.append(c.get());
+	if (c = preparePromisc(adapter))
+		result.append(c.get());
 
-	boost::optional <Libvirt::Domain::Xml::FilterrefNodeAttributes> filterref_domain =
-			Device::Network::View(adapter).getPredefinedFilterref();
-
-	if (!filterref_domain)
-		return result;
-
-	Libvirt::Filter::Xml::FilterrefNodeAttributes filterref;
-
-	QList <Libvirt::Filter::Xml::Parameter> params;
-	foreach(const Libvirt::Domain::Xml::Parameter &param_domain, filterref_domain->getParameterList())
-		params.append(prepareNetFiltersParam(param_domain));
-
-	filterref.setFilter(filterref_domain->getFilter());
-	filterref.setParameterList(params);
-
-	mpl::at_c<Libvirt::Filter::Xml::VChoice5120::types, 0>::type filterref_holder;
-	filterref_holder.setValue(filterref);
-	filterref.setFilter(filterref_domain->getFilter());
-
-	result.append(Libvirt::Filter::Xml::VChoice5120(filterref_holder));
 	return result;
-}
-
-Libvirt::Filter::Xml::Parameter Reverse::prepareNetFiltersParam(
-		const Libvirt::Domain::Xml::Parameter &value)
-{
-	Libvirt::Filter::Xml::Parameter param;
-	param.setName(value.getName());
-	param.setValue(value.getValue());
-	return param;
 }
 
 QList <Libvirt::Filter::Xml::Tcp> Reverse::prepareTcp(
