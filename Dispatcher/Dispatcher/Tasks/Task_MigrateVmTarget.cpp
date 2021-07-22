29c6760419b23703bf9310ab2fbae269ded0ffd9
Dmitry Mishin <dim@virtuozzo.com>
2021-07-22 19:13:32 +0300

    Revert "QCOW2 images for VMs should be created with subclusters enabled #PSBM-131094"
    This reverts commit ba14b5b01d00ffa33901c757817f6c8be94b8167.
    Temporary revert subclusters enablement due to problems in QEMU

--- Dispatcher/Dispatcher/Tasks/Task_MigrateVmTarget.cpp
+++ Dispatcher/Dispatcher/Tasks/Task_MigrateVmTarget.cpp
@@ -966,7 +966,7 @@ void Frontend::create(const CVmHardDisk& event_)
 	}
 	QStringList a;
 	a << "create" << "-f" << "qcow2";
-	a << "-o" << "cluster_size=1M,lazy_refcounts=on,extended_l2=on";
+	a << "-o" << "cluster_size=1M,lazy_refcounts=on";
 	a << event_.getSystemName();
 	a << QString::number(event_.getSize()).append("M");
 
