OpenVZ/prl-disp-service

commit 51adaa9275c69c67855f12d7e8de61ec20268a49

No need to set currentMemory
currentMemory is volatile, there is no need to set it to migration
target configuration.

Fixes #PSBM-47270

Libraries/Transponster/Reverse.cpp
@@ -1641,6 +1641,12 @@ PRL_RESULT Fixer::setDevices()
 	return PRL_ERR_SUCCESS;
 }
 
+PRL_RESULT Fixer::setResources(const VtInfo&)
+{
+	m_result->setCurrentMemory(boost::none);
+	return PRL_ERR_SUCCESS;
+}
+
 } // namespace Reverse
 } // namespace Vm
 

Libraries/Transponster/Reverse.h
@@ -234,10 +234,7 @@ struct Fixer: Builder
 	{
 		return PRL_ERR_SUCCESS;
 	}
-	PRL_RESULT setResources(const VtInfo&)
-	{
-		return PRL_ERR_SUCCESS;
-	}
+	PRL_RESULT setResources(const VtInfo&);
 	PRL_RESULT setDevices();
 };
 

commit 1b09a6ca5393cb0a75b90c72565f51fb741e8d0e

PSBM-132009 Preserve VM currentMemory setting on migration
The issue introduced by commit 51adaa9275c. The commit was OK when dispatcher
was the primary source of information about VM's memory size, but nowadays -
this prerequirement is broken - libvirt is the primary source. We need to
preserve inactive currentMemory value on migration.

Dispatcher/Dispatcher/CDspLibvirt.cpp
@@ -422,7 +422,8 @@ Prl::Expected<QString, Error::Simple> Config::fixup(const CVmConfiguration& valu
 	if (i.isFailed())
 		return i.error();
 
-	Transponster::Vm::Reverse::Fixer u(value_, read_());
+	Transponster::Vm::Reverse::Fixer u(value_, read_(),
+						m_flags & VIR_DOMAIN_XML_INACTIVE);
 	PRL_RESULT res = Transponster::Director::domain(u, i.value());
 	if (PRL_FAILED(res))
 		return Error::Simple(res);

Libraries/Transponster/Reverse.cpp
@@ -2417,7 +2417,8 @@ PRL_RESULT Mixer::setResources(const VtInfo& info_)
 ///////////////////////////////////////////////////////////////////////////////
 // struct Fixer
 
-Fixer::Fixer(const CVmConfiguration& input_, char* xml_): Builder(input_)
+Fixer::Fixer(const CVmConfiguration& input_, char* xml_, bool inactive_):
+	Builder(input_), m_inactive(inactive_)
 {
 	shape(xml_, m_result);
 }
@@ -2489,7 +2490,8 @@ PRL_RESULT Fixer::setDevices()
 
 PRL_RESULT Fixer::setResources(const VtInfo&)
 {
-	m_result->setCurrentMemory(boost::none);
+	if (!m_inactive)
+		m_result->setCurrentMemory(boost::none);
 	return PRL_ERR_SUCCESS;
 }
 

Libraries/Transponster/Reverse.h
@@ -248,7 +248,7 @@ struct Mixer: Builder
 
 struct Fixer: Builder
 {
-	Fixer(const CVmConfiguration& input_, char* xml_);
+	Fixer(const CVmConfiguration& input_, char* xml_, bool inactive_);
 
 	PRL_RESULT setBlank();
 	PRL_RESULT setIdentification();
@@ -258,6 +258,9 @@ struct Fixer: Builder
 	}
 	PRL_RESULT setResources(const VtInfo&);
 	PRL_RESULT setDevices();
+
+private:	
+	bool m_inactive;
 };
 
 ///////////////////////////////////////////////////////////////////////////////