d10bb95b02b4e1ab2586f6d64f556f22c8bbec50
Pavel Vokhmyanin <pvokhmyanin@virtuozzo.com>
2022-02-01 12:45:11 +0300

    Update OVMF paths #PSBM-137859
    OVMF from vhs9 has new paths for non-secboot OVMF images, now
    they are refered as "piix" images. We should update paths.
    
    Signed-off-by: Pavel Vokhmyanin <pvokhmyanin@virtuozzo.com>

--- Libraries/Virtuozzo/OvmfHelper.cpp
+++ Libraries/Virtuozzo/OvmfHelper.cpp
@@ -5,7 +5,7 @@
 QString OVMF::getTemplate(Chipset_type machine_type)
 {
 	return machine_type == Chipset_type::Q35 ? "/usr/share/OVMF/OVMF_VARS.secboot.fd":
-											   "/usr/share/OVMF/OVMF_VARS.fd";
+											   "/usr/share/OVMF/OVMF_VARS.piix.fd";
 }
 
 //return firmware for vz8 and vz7
@@ -13,5 +13,5 @@ QString OVMF::getTemplate(Chipset_type machine_type)
 QString OVMF::getFirmware(Chipset_type machine_type)
 {
 	return machine_type == Chipset_type::Q35 ? "/usr/share/OVMF/OVMF_CODE.secboot.fd":
-											   "/usr/share/OVMF/OVMF_CODE.fd";
+											   "/usr/share/OVMF/OVMF_CODE.piix.fd";
 }
