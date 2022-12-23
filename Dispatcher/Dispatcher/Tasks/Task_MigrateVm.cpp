OpenVZ/prl-disp-service

commit 6b60bc2431f2d7ae27901d8692d5c2c1a9dec93e

bug #PSBM-93985 aka VM hang on migrate state on source and dest node, after kill qemu-kvm
qemu starts retrying connection after a disconnect. it hangs waiting
for dialog if an attempt succeeds.
let's close the listening socket when its corresponding pump has
finished with success.

Dispatcher/Dispatcher/Tasks/Task_MigrateVm.cpp
@@ -1001,7 +1001,7 @@ void Frontend::accept(const client_type& connection_)
 bool Frontend::setup(const char* method_)
 {
 	bool x;
-	serverList_type::value_type s(new QTcpServer());
+	listenerMap_type::mapped_type s(new QTcpServer());
 	x = getConnector()->connect(s.data(), SIGNAL(newConnection()), method_);
 	if (!x)
 	{
@@ -1014,7 +1014,7 @@ bool Frontend::setup(const char* method_)
 		WRITE_TRACE(DBG_FATAL, "can't listen");
 		return false;
 	}
-	m_servers << s;
+	m_listenerMap[method_] = s;
 	return true;
 }
 
@@ -1025,7 +1025,7 @@ void Frontend::on_entry(const Event& event_, FSM& fsm_)
 	getConnector()->setService(m_service);
 	if (setup(SLOT(acceptLibvirt())) && setup(SLOT(acceptQemuState()))
 		&& setup(SLOT(acceptQemuDisk())))
-		return getConnector()->handle(m_servers);
+		return getConnector()->handle(m_listenerMap.values());
 
 	fsm_.process_event(Flop::Event(PRL_ERR_FAILURE));
 }
@@ -1034,19 +1034,44 @@ template <typename Event, typename FSM>
 void Frontend::on_exit(const Event& event_, FSM& fsm_)
 {
 	vsd::Frontend<Frontend>::on_exit(event_, fsm_);
-	foreach (const QSharedPointer<QTcpServer>& s, m_servers)
+	foreach (const QString& alias_, m_listenerMap.keys())
 	{
-		s->disconnect(SIGNAL(newConnection()),
-				getConnector());
-		s->close();
+		closeListener(qPrintable(alias_));
 	}
 	foreach (const client_type& c, m_clients)
 	{
 		c->close();
 	}
 	m_clients.clear();
-	m_servers.clear();
+	m_listenerMap.clear();
 	getConnector()->setService(NULL);
+}
+
+void Frontend::closeListener(const char* alias_)
+{
+	QString k(alias_);
+	if (!m_listenerMap.contains(k))
+		return;
+
+	listenerMap_type::mapped_type s = m_listenerMap.value(k);
+	if (!s.isNull() && s->isListening())
+	{
+		s->disconnect(SIGNAL(newConnection()),
+				getConnector());
+		s->close();
+	}
+}
+
+void Frontend::closeDiskListener(const qemuDisk_type::Down& event_)
+{
+	Q_UNUSED(event_);
+	closeListener(SLOT(acceptQemuDisk()));
+}
+
+void Frontend::closeStateListener(const qemuState_type::Down& event_)
+{
+	Q_UNUSED(event_);
+	closeListener(SLOT(acceptQemuState()));
 }
 
 } // namespace Tunnel

Dispatcher/Dispatcher/Tasks/Task_MigrateVmSource_p.h
@@ -645,7 +645,7 @@ private:
 struct Frontend: vsd::Frontend<Frontend>, Vm::Connector::Mixin<Connector>
 {
 	typedef QSharedPointer<QTcpSocket> client_type;
-	typedef Libvirt::State::serverList_type serverList_type;
+	typedef QHash<QString, Libvirt::State::serverList_type::value_type> listenerMap_type;
 	typedef Pump::Frontend<Machine_type, Vm::Tunnel::libvirtChunk_type::s_command>
 		libvirt_type;
 	typedef Qemu::Hub
@@ -691,12 +691,18 @@ struct Frontend: vsd::Frontend<Frontend>, Vm::Connector::Mixin<Connector>
 
 	void accept(const client_type& connection_);
 
+	void closeDiskListener(const qemuDisk_type::Down& event_);
+
+	void closeStateListener(const qemuState_type::Down& event_);
+
 	struct transition_table : boost::mpl::vector<
 		msmf::Row<initial_state,       Flop::Event,      Flop::State>,
 		a_irow<initial_state,          client_type,      &Frontend::accept>,
 		msmf::Row<initial_state,       Vm::Pump::Launch_type,
 			pumpingState_type::entry_pt<essence_type::Entry> >,
 		a_irow<pumpingState_type,      client_type,      &Frontend::accept>,
+		a_irow<pumpingState_type,      qemuDisk_type::Down, &Frontend::closeDiskListener>,
+		a_irow<pumpingState_type,      qemuState_type::Down, &Frontend::closeStateListener>,
 		msmf::Row<pumpingState_type
 			::exit_pt<Success>,    msmf::none,       Success>,
 		msmf::Row<pumpingState_type
@@ -708,8 +714,10 @@ struct Frontend: vsd::Frontend<Frontend>, Vm::Connector::Mixin<Connector>
 private:
 	bool setup(const char* method_);
 
+	void closeListener(const char* alias_);
+
 	IO* m_service;
-	serverList_type m_servers;
+	listenerMap_type m_listenerMap;
 	QList<QSharedPointer<QTcpSocket> > m_clients;
 };
 

Dispatcher/Dispatcher/Tasks/Task_MigrateVmTarget_p.h
@@ -469,6 +469,8 @@ struct Frontend: vsd::Frontend<Frontend>, Vm::Connector::Mixin<Vm::Target::Conne
 			essenceState_type::entry_pt<essence_type::Entry> >,
 		a_row<essenceState_type
 			::exit_pt<Success>,    msmf::none,  Success, &Frontend::synch>,
+		msmf::Row<essenceState_type,   qemuDisk_type::Down, msmf::none>,
+		msmf::Row<essenceState_type,   qemuState_type::Down, msmf::none>,
 		msmf::Row<essenceState_type
 			::exit_pt<Flop::State>,Flop::Event, Flop::State>
 	>

Dispatcher/Dispatcher/Tasks/Task_MigrateVm_p.h
@@ -1358,6 +1358,9 @@ struct Unit: vsd::Trace<T>, Vm::Connector::Mixin<typename U::machine_type>
 	struct Good
 	{
 	};
+	struct Down
+	{
+	};
 	struct Action
 	{
 		template<class M>
@@ -1385,7 +1388,10 @@ struct Unit: vsd::Trace<T>, Vm::Connector::Mixin<typename U::machine_type>
 			p->stop();
 			state_.m_pumpMap.remove(k);
 			if (state_.m_pumpMap.isEmpty())
+			{
 				fsm_.process_event(Good());
+				state_.getMachine().process_event(Down());
+			}
 		}
 	};
 

commit 93cc7832def1271347bfc87477471d3b7b084db9

VmMigrate: listner list has to be in right order #PSBM-141219
QHash::values() returns a list containing all the values in the hash,
in an arbitrary order but dispatcher architecture relies on specific.
It was broken in commit 6b60bc2431f2 (#PSBM-93985). This fix restores
old order.

Signed-off-by: Aleksandr Leskin <aleksandr.leskin@virtuozzo.com>

Dispatcher/Dispatcher/Tasks/Task_MigrateVm.cpp
@@ -1014,7 +1014,13 @@ void Frontend::on_entry(const Event& event_, FSM& fsm_)
 	getConnector()->setService(m_service);
 	if (setup(SLOT(acceptLibvirt())) && setup(SLOT(acceptQemuState()))
 		&& setup(SLOT(acceptQemuDisk())))
-		return getConnector()->handle(m_listenerMap.values());
+	{
+		Libvirt::State::serverList_type listnerList;
+		listnerList.push_back(m_listenerMap[SLOT(acceptLibvirt())]);
+		listnerList.push_back(m_listenerMap[SLOT(acceptQemuState())]);
+		listnerList.push_back(m_listenerMap[SLOT(acceptQemuDisk())]);
+		return getConnector()->handle(listnerList);
+	}
 
 	fsm_.process_event(Flop::Event(PRL_ERR_FAILURE));
 }