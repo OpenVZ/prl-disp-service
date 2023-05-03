b34721c4756b00dcc054f2bb5b9e918b020b166d
Aleksandr Leskin <aleksandr.leskin@virtuozzo.com>
2023-05-03 21:57:39 +0300

    build fix: after cherry-pick from VZ7 #PSBM-146838
    fix broken build in '3dc607253cf':
    'nvram: append boot disk of VM to script for upgrading NVRAM'
    
    Signed-off-by: Aleksandr Leskin <aleksandr.leskin@virtuozzo.com>

--- Libraries/Virtuozzo/OvmfHelper.cpp
+++ Libraries/Virtuozzo/OvmfHelper.cpp
@@ -291,9 +291,9 @@ bool NvramUpdater::isOldVerison()
 		return false;
 	}
 
-	if (static_cast<Chipset_type>(m_input.getVmHardwareList()->getChipset()->getType()) == Chipset_type::Q35)
+	if (static_cast<Chipset_type>(m_input.getVmHardwareList()->getChipset()->getType()) == Chipset_type::CHIP_Q35)
 	{
-		WRITE_TRACE(DBG_FATAL, "NVRAM Updater: VM with Q35 chipset does not need update NVRAM file",
+		WRITE_TRACE(DBG_FATAL, "NVRAM Updater: VM with Q35 chipset does not need update NVRAM file '%s'",
 					QSTR2UTF8(m_oldNvram.filePath()));
 		return false;
 	}
