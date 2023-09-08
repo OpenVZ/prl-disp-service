37f07f613161ca20f13aea0d661469839f2a51e0
Aleksandr Leskin <aleksandr.leskin@virtuozzo.com>
2023-09-08 08:48:01 +0200

    transponster: all appended CD devices should have boot order #PSBM-150310
    When VM boots on UEFI BIOS (edk2-ovmf) and has a cdrom attached via IDE bus,
    it may not see the device. In particular, this blocks us from installing
    the guest tools or upgrade OS.
    So let's add boot order attribute every time we attach CDROM to a VM.
    This patch affects the running VM (live migration and installtools).
    
    Signed-off-by: Aleksandr Leskin <aleksandr.leskin@virtuozzo.com>

--- Libraries/Transponster/Reverse.cpp
+++ Libraries/Transponster/Reverse.cpp
@@ -3057,7 +3057,7 @@ namespace Boot
 ///////////////////////////////////////////////////////////////////////////////
 // struct Reverse
 
-Reverse::Reverse(const QList<device_type* >& list_)
+Reverse::Reverse(const QList<device_type* >& list_) : m_extra_index(0)
 {
 	unsigned o = 0;
 	std::list<device_type* > x = list_.toStdList();
@@ -3073,12 +3073,16 @@ Reverse::Reverse(const QList<device_type* >& list_)
 	}
 }
 
-Reverse::order_type Reverse::operator()(const CVmDevice& device_) const
+Reverse::order_type Reverse::operator()(const CVmDevice& device_)
 {
 	key_type k = qMakePair(device_.getDeviceType(), device_.getIndex());
 	map_type::const_iterator p = m_map.find(k);
 	if (m_map.end() == p)
+	{
+		if (device_.getDeviceType() == PDE_OPTICAL_DISK)
+			return m_map.size() + (++m_extra_index);
 		return order_type();
+	}
 
 	return p.value();
 }
