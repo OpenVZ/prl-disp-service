OpenVZ/prl-disp-service

commit 560455baf80f05d1afebe5ca7d3cb3c86052105b

backup: append log to watch for list and last backup #PSBM-156006
Signed-off-by: Aleksandr Leskin <aleksandr.leskin@virtuozzo.com>

Dispatcher/Dispatcher/Tasks/Task_BackupHelper.cpp
@@ -1746,6 +1746,10 @@ BackupItem* Task_BackupMixin::getLastBaseBackup(const QString &sVmUuid,
 			a.reset(new BackupItem(b.value()));
 		}
 	}
+
+	WRITE_TRACE(DBG_FATAL, "getLastBaseBackup [%s]; for vm '%s' from list: [%s]",
+			QSTR2UTF8(a->getUuid()), QSTR2UTF8(sVmUuid), QSTR2UTF8(lstBackupUuid.join("; ")));
+
 	return a.release();
 }
 

commit 52c245f4105df82408a6fba39a1567066dcb6b83

backup: fix printing backup list for case without backups #PSBM-156151
problem was introduced by commit: 560455baf80
backup: append log to watch for list and last backup #PSBM-156006

Signed-off-by: Aleksandr Leskin <aleksandr.leskin@virtuozzo.com>

Dispatcher/Dispatcher/Tasks/Task_BackupHelper.cpp
@@ -1748,7 +1748,8 @@ BackupItem* Task_BackupMixin::getLastBaseBackup(const QString &sVmUuid,
 	}
 
 	WRITE_TRACE(DBG_FATAL, "getLastBaseBackup [%s]; for vm '%s' from list: [%s]",
-			QSTR2UTF8(a->getUuid()), QSTR2UTF8(sVmUuid), QSTR2UTF8(lstBackupUuid.join("; ")));
+			a.get() ? QSTR2UTF8(a->getUuid()) : "Empty",
+					QSTR2UTF8(sVmUuid), QSTR2UTF8(lstBackupUuid.join("; ")));
 
 	return a.release();
 }